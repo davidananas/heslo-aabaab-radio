f072021489b3bcdba0839301250d711372df1fe2
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-05 16:32:29 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,13 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
         pins.servoWritePin(AnalogPin.P0, 90)
         basic.showIcon(IconNames.Happy)
-        basic.showString("GOOD")
+        basic.pause(500)
+        basic.clearScreen()
     } else {
         if (receivedNumber == 3) {
             basic.showIcon(IconNames.Sad)
-            basic.showString("FALSE")
+            basic.pause(500)
+            basic.clearScreen()
         } else {
             if (receivedNumber == 4) {
                 pins.servoWritePin(AnalogPin.P0, 0)
@@ -21,9 +23,11 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (heslo == USTUP) {
         radio.sendNumber(2)
+        basic.showString("GOOD")
         basic.showIcon(IconNames.Happy)
     } else {
         radio.sendNumber(3)
+        basic.showString("FALSE")
         basic.showIcon(IconNames.Sad)
     }
     basic.pause(500)
@@ -33,12 +37,13 @@ input.onButtonPressed(Button.B, function () {
     USTUP = "" + USTUP + "B"
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    pins.servoWritePin(AnalogPin.P0, 0)
     radio.sendNumber(4)
 })
 let USTUP = ""
 let heslo = ""
 heslo = "AABAAB"
-pins.servoWritePin(AnalogPin.P0, 0)
+radio.sendNumber(4)
 basic.forever(function () {
     radio.setGroup(5)
 })
